5d1ed64005497fabc8d2ceb01dc65a5c3d3ede9c
Hawk Itzme <smquasim016@gmail.com>
2023-03-10 00:03:34 +0530

    Video View to play any video from internet using URl, code added

--- app/src/main/java/com/example/video_playerapp/MainActivity.kt
+++ app/src/main/java/com/example/video_playerapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.video_playerapp
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
 import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +26,20 @@ class MainActivity : AppCompatActivity() {
         mediaController.setMediaPlayer(videoView)
         videoView.setMediaController(mediaController)
         videoView.start()
+
+        // Displaying videos from internet
+        val videoView2 : VideoView = findViewById(R.id.videoview2)
+
+        val uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/002/231/original/5226496.mp4")
+
+        videoView2.setVideoURI(uri)
+
+        // Media Controller
+        val mediaController2 = MediaController(this)
+
+        mediaController2.setAnchorView(videoView2)
+        mediaController2.setMediaPlayer(videoView2)
+        videoView2.setMediaController(mediaController2)
+        videoView2.start()
     }
 }
